eed4b6e2921f99d3bd31073a1f3226e189f18ab8
annapmarin <annapratsmarin@gmail.com>
2023-05-28 19:13:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      {/* My balance $921.48 Spending - Last 7 days mon tue wed thu fri sat sun Total this month $478.33 +2.4% from last month  */}
+    </>
+  )
+}
+
+export default App
