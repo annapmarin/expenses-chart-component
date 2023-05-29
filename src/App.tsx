290b73f8f05c911546e71d294aa9331a625a75e4
annapmarin <annapratsmarin@gmail.com>
2023-05-29 11:43:54 +0200

    mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-
+import Logo from "./assets/img/logo.svg";
+import Chart from "./components/Chart";
 
 function App() {
-
   return (
     <>
-      {/* My balance $921.48 Spending - Last 7 days mon tue wed thu fri sat sun Total this month $478.33 +2.4% from last month  */}
+      <main>
+        <div className="container">
+          <div className="container__header">
+            <div className="container__subheader">
+              <p>My balance</p>
+              <span className="container__title">$921.48</span>
+            </div>
+            <img className="container__logo" src={Logo} alt="" />
+          </div>
+          <div className="container__content">
+            <h1 className="container__title">Spending - Last 7 days</h1>
+            <div className="container__bars">
+              <Chart />
+            </div>
+            <hr />
+            <div className="container__stats">
+              <p>Total this month</p>
+              <div>
+                <span className="container__total">$478.33</span>
+                <div>
+                  <span>+2.4%</span>
+                  <p>from last month</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Chart.tsx
@@ -0,0 +1,46 @@
+import { CSSProperties, useState } from "react";
+import ChartData from "../../data.json";
+
+export default function Chart() {
+  const [hoveredBar, setHoveredBar] = useState<number | null>(null);
+  const highestAmount = Math.max(...ChartData.map(data => data.amount));
+  const highestAmountIndex = ChartData.findIndex(data => data.amount === highestAmount);
+
+  function barsStyle(amount: number, index: number): CSSProperties {
+    const baseStyle: CSSProperties = {
+      height: amount * 3 + "px",
+    };
+
+    if (index === highestAmountIndex && hoveredBar === index) {
+      return {
+        ...baseStyle,
+        backgroundColor: "var(--cyan)",
+      };
+    }
+
+    return baseStyle;
+  }
+
+  return (
+    <>
+      <div className="chart">
+        <div className="chart__bars">
+          {ChartData.map((data, index) => {
+            return (
+              <div className="chart__data" key={index}>
+                <p className={hoveredBar === index ? "show" : "hide"}>${data.amount}</p>
+                <div
+                  className="chart__bar"
+                  style={barsStyle(data.amount, index)}
+                  onMouseEnter={() => setHoveredBar(index)}
+                  onMouseLeave={() => setHoveredBar(null)}
+                ></div>
+                <span>{data.day}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
